58f42d316b39802e2727d98af1c05f21f75e212d
apatron100 <30960286+apatron100@users.noreply.github.com>
2018-02-19 01:32:49 -0800

    added MongoDb and Mongoose and data base

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require('express')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
 var app = express()
 var PORT = process.env.PORT || 5000
 
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}))
+//parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static("client/build"))
+
+//mongoose connection
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/mern=crude')
+//Routes
 app.get('/', function (req, res) {
-  res.json({'message': 'Hello World'})
+	res.sendFile(path.join(_dirname + 'client/build/index.html'))
 })
 
 app.listen(PORT, function(){
